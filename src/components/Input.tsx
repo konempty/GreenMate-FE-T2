21c10442691883efae028b078239ddf86e34edf7
KimTaeho <xoghrla0311@gmail.com>
2025-07-14 20:03:46 +0900

    TEAM2-14 제미나이 2차 리뷰 반영 및 Lint 반영

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 
 interface InputProps {
-    type: string;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    placeholder?: string;
+  id?: string;
+  type: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
 }
 
-const Input: React.FC<InputProps> = ({ type, value, onChange, placeholder }) => {
-    return (
-        <input 
-            type={type}
-            value={value}
-            onChange={onChange}
-            placeholder={placeholder}
-        />
-    );
+const Input: React.FC<InputProps> = ({
+  id,
+  type,
+  value,
+  onChange,
+  placeholder,
+}) => {
+  return (
+    <input
+      id={id}
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
 };
 
 export default Input;
