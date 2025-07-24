6131726c35e0c0be33eeec704980130ab3bd1a56
seonnnn <jjinjuu317@gmail.com>
2025-07-25 00:02:14 +0900

    TEAM2-66 /header 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/header" element={<Header />} />
       </Routes>
     </BrowserRouter>
   );
