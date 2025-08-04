26e963e0641032864abbc4bfd3bd9b8ae7e52fd0
seonnnn <jjinjuu317@gmail.com>
2025-08-04 19:52:58 +0900

    TEAM2-67 footer경로 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Post from "./pages/Post";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Education from "./pages/Education";
-import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/recycling-education" element={<Education />} />
-        <Route path="/footer" element={<Footer />} />
       </Routes>
     </BrowserRouter>
   );
